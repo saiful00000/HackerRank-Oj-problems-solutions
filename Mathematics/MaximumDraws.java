2f96f8935d703aff5899f4c2e7cb871a173908cd
saiful00000 <>
2018-05-31 02:54:50 +0600

    "may 31 - 2018"

--- Mathematics/MaximumDraws.java
+++ Mathematics/MaximumDraws.java
@@ -1,4 +1,20 @@
 package Mathematics;
+import java.util.Scanner;
 
 public class MaximumDraws {
+
+    static void maxDraws(int n) {
+        int worstCaseRemove = n+1;
+        System.out.println(worstCaseRemove);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int t = in.nextInt();
+        for (int i=0; i<t; i++){
+            int n = in.nextInt();
+            maxDraws(n);
+        }
+    }
 }
